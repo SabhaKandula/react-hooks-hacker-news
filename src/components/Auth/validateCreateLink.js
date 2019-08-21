35743945ccdb4d54d26f6d83c6bbc1bcc18d962e
Sabha Kandula <sabha.kandula@gmail.com>
2019-08-21 15:35:26 -0400

    Create Links functionality and validation

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -1,1 +1,17 @@
-export default function validateCreateLink(values) {}
+export default function validateCreateLink(values) {
+  let errors = {};
+  //description Errors
+  if (!values.description) {
+    errors.description = "Description required";
+  } else if (values.descriptio.length < 10) {
+    errors.description = "Description must be atleast 10 characterss";
+  }
+
+  //URL Errors
+  if (!values.url) {
+    errors.url = "Password required";
+  } else if (!/^(ftp | http | https): \/\/[^ "]+$/.test(values.url)) {
+    errors.url = "URL must be valid";
+  }
+  return errors;
+}
--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,7 +1,45 @@
 import React from "react";
+import useFormValidation from "../Auth/useFormValidation";
+import validateCreateLink from "../Auth/validateCreateLink";
 
+const INITITAL_STATE = {
+  description: "",
+  url: ""
+};
 function CreateLink(props) {
-  return <div>CreateLink</div>;
+  const { handleSubmit, handleChange, values, errors } = useFormValidation(
+    INITITAL_STATE,
+    validateCreateLink,
+    handleCreateLink
+  );
+
+  function handleCreateLink() {}
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-column mt3">
+      <input
+        onChange={handleChange}
+        value={values.description}
+        type="text"
+        autoComplete="off"
+        placeholder="A description for your link"
+        name="description"
+        className={errors.description && "error-input"}
+      />
+      {errors.description && <p className="error-text">{errors.description}</p>}
+      <input
+        onChange={handleChange}
+        value={values.url}
+        type="text"
+        autoComplete="off"
+        placeholder="URL for your link"
+        name="url"
+      />
+      {errors.url && <p className="error-text">{errors.url}</p>}
+      <button className="button" type="submit">
+        submit
+      </button>
+    </form>
+  );
 }
 
 export default CreateLink;
