b1bc3a05ac5638decbdab8754aa1b672ee145bf3
Sabha Kandula <sabha.kandula@gmail.com>
2019-08-21 12:04:28 -0400

    Form input validations and firebase config

--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,8 +1,22 @@
 import React from "react";
+import validateLogin from "./validateLogin";
 
 function useFormValidation(initialstate) {
   const [values, setValues] = React.useState(initialstate);
+  const [errors, setErrors] = React.useState({});
+  const [isSubmitting, setSubmitting] = React.useState(false);
 
+  React.useEffect(() => {
+    if (isSubmitting) {
+      const noErrors = Object.keys(errors).length === 0;
+      if (noErrors) {
+        console.log("authenicated", values);
+        setSubmitting(false);
+      } else {
+        setSubmitting(false); 
+      }
+    }
+  }, [errors]);
   function handleChange(event) {
     event.persist();
     setValues(previousValues => ({
@@ -10,11 +24,18 @@ function useFormValidation(initialstate) {
       [event.target.name]: event.target.value
     }));
   }
+  function handleBlur(event) {
+    const validationErrors = validateLogin(values);
+    setErrors(validationErrors);
+  }
   function handleSubmit(event) {
     event.preventDefault();
+    const validationErrors = validateLogin(values);
+    setErrors(validationErrors);
+    setSubmitting(true);
     console.log({ values });
   }
-  return { handleChange, handleSubmit, values };
+  return { handleChange, handleSubmit, handleBlur, values,errors, isSubmitting };
 }
 
 export default useFormValidation;
--- src/components/Auth/validateLogin.js
+++ src/components/Auth/validateLogin.js
@@ -1,1 +1,17 @@
-export default function validateLogin(values) {}
+export default function validateLogin(values) {
+  let errors = {};
+  //Email Errors
+  if (!values.email) {
+    errors.email = "Email required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = "Invalid Email address";
+  }
+
+  //Password Errors
+  if (!values.password) {
+    errors.password = "Password required";
+  } else if (values.password.length < 6) {
+    errors.password = "Password must be atleast 6 characters";
+  }
+  return errors;
+}
