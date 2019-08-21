b0273b5c86d973da19fdeae112bc95a340c49364
Sabha Kandula <sabha.kandula@gmail.com>
2019-08-21 15:50:28 -0400

    Creating Firestore and add link to the collection

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -3,14 +3,14 @@ export default function validateCreateLink(values) {
   //description Errors
   if (!values.description) {
     errors.description = "Description required";
-  } else if (values.descriptio.length < 10) {
+  } else if (values.description.length < 10) {
     errors.description = "Description must be atleast 10 characterss";
   }
 
   //URL Errors
   if (!values.url) {
     errors.url = "Password required";
-  } else if (!/^(ftp | http | https): \/\/[^ "]+$/.test(values.url)) {
+  } else if (!/^(ftp|http|https):\/\/[^ "]+$/.test(values.url)) {
     errors.url = "URL must be valid";
   }
   return errors;
--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,19 +1,40 @@
 import React from "react";
 import useFormValidation from "../Auth/useFormValidation";
 import validateCreateLink from "../Auth/validateCreateLink";
+import { FirebaseContext } from "../../firebase";
 
 const INITITAL_STATE = {
   description: "",
   url: ""
 };
 function CreateLink(props) {
+  const { firebase, user } = React.useContext(FirebaseContext);
   const { handleSubmit, handleChange, values, errors } = useFormValidation(
     INITITAL_STATE,
     validateCreateLink,
     handleCreateLink
   );
 
-  function handleCreateLink() {}
+  function handleCreateLink() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      const { url, description } = values;
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName
+        },
+        votes: [],
+        comments: [],
+        created: Date.now()
+      };
+      firebase.db.collection("links").add(newLink);
+      props.history.push("/");
+    }
+  }
   return (
     <form onSubmit={handleSubmit} className="flex flex-column mt3">
       <input
@@ -29,7 +50,7 @@ function CreateLink(props) {
       <input
         onChange={handleChange}
         value={values.url}
-        type="text"
+        type="url"
         autoComplete="off"
         placeholder="URL for your link"
         name="url"
