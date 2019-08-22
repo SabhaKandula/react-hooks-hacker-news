649dcc0e8bfdecf23cd0748ab4c94de6fd92d038
Sabha Kandula <sabha.kandula@gmail.com>
2019-08-22 11:48:13 -0400

    Pagination

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,28 +1,44 @@
 import React from "react";
 import { FirebaseContext } from "../../firebase";
 import LinkItem from "./LinkItem";
+import { LINKS_PER_PAGE } from "../../utils";
 
 function LinkList(props) {
   const { firebase } = React.useContext(FirebaseContext);
   const [links, setLinks] = React.useState([]);
-  //const isNewPage = props.location.pathname.includes("new");
+  const isNewPage = props.location.pathname.includes("new");
   const isTopPage = props.location.pathname.includes("top");
+  const [cursor, setCursor] = React.useState(null);
+  const page = Number(props.match.params.page);
   React.useEffect(() => {
     const unsubscribe = getLinks();
     return () => unsubscribe();
-  }, [isTopPage]);
+  }, [isTopPage, page]);
+
   function getLinks() {
+    const hasCursor = Boolean(cursor);
     if (isTopPage) {
       return firebase.db
         .collection("links")
         .orderBy("voteCount", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (page === 1) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (hasCursor) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .startAfter(cursor.created)
+        .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
     }
-    return firebase.db
-      .collection("links")
-      .orderBy("created", "desc")
-      .onSnapshot(handleSnapshot);
   }
+
   function handleSnapshot(snapshot) {
     const links = snapshot.docs.map(doc => {
       return {
@@ -31,8 +47,20 @@ function LinkList(props) {
       };
     });
     setLinks(links);
+    const lastlink = links[links.length - 1];
+    setCursor(lastlink);
   }
-
+  function visitPreviousPage() {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`);
+    }
+  }
+  function visitNextPage() {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`);
+    }
+  }
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
   return (
     <div>
       {links.map((link, index) => (
@@ -40,9 +68,19 @@ function LinkList(props) {
           key={link.id}
           showCount={true}
           link={link}
-          index={index + 1}
+          index={index + pageIndex}
         />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPreviousPage}>
+            Previous
+          </div>
+          <div className="pointer" onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 }
